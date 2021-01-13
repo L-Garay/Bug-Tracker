a81432a7b752d1d4a950507092df08511314d43c
Logan Garay <garay.logan@gmail.com>
2021-01-13 16:01:00 -0700

    updated axios

--- server/controllers/BugsController.js
+++ server/controllers/BugsController.js
@@ -1,18 +1,18 @@
-import express from "express";
-import bugService from "../services/BugService";
-import noteService from "../services/NoteService";
+import express from 'express';
+import bugService from '../services/BugService';
+import noteService from '../services/NoteService';
 
 export default class BugController {
   constructor() {
     this.router = express
       .Router()
       //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
-      .get("", this.getAll)
-      .get("/:id", this.getBugById)
-      .get("/:id/notes", this.getNotesByBugId)
-      .post("", this.createBug)
-      .put("/:id", this.editBug)
-      .delete("/:id", this.closeBug);
+      .get('', this.getAll)
+      .get('/:id', this.getBugById)
+      .get('/:id/notes', this.getNotesByBugId)
+      .post('', this.createBug)
+      .put('/:id', this.editBug)
+      .delete('/:id', this.closeBug);
   }
 
   async getAll(req, res, next) {
@@ -23,12 +23,12 @@ export default class BugController {
       next(error);
     }
   }
-  async getBugById(req, res, netx) {
+  async getBugById(req, res, next) {
     try {
       let data = await bugService.getBugById(req.params.id);
       return res.send(data);
     } catch (error) {
-      netx(error);
+      next(error);
     }
   }
   async getNotesByBugId(req, res, next) {
